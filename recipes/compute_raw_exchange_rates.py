efe6a489ee0e53357ffc02c150909ef48a55a67a
krm.oussaidi@gmail.com <krm.oussaidi@gmail.com>
2026-09-08 13:46:06 +0000

    Saved recipe: ECOMMERCE_DATA_ENGINEERING.compute_raw_exchange_rates

--- recipes/compute_raw_exchange_rates.py
+++ recipes/compute_raw_exchange_rates.py
@@ -1,20 +1,25 @@
-"base_currency"import requests
+import requests
 import pandas as pd
 import dataiku
 
-url = "https://api.frankfurter.dev/v1/latest?base=BRL&symbols=EUR"
+url = "https://api.frankfurter.dev/v1/2016-01-01..2018-12-31?base=BRL&symbols=EUR"
 
 response = requests.get(url)
 response.raise_for_status()
 
 data = response.json()
 
-df = pd.DataFrame([{
-    "date": data["date"],
-    "base_currency": data["base"],
-    "target_currency": "EUR",
-    "exchange_rate": data["rates"]["EUR"]
-}])
+rows = []
+
+for date, rates in data["rates"].items():
+    rows.append({
+        "date": date,
+        "base_currency": "BRL",
+        "target_currency": "EUR",
+        "exchange_rate": rates["EUR"]
+    })
+
+df = pd.DataFrame(rows)
 
 output_dataset = dataiku.Dataset("raw_exchange_rates")
 output_dataset.write_with_schema(df)
