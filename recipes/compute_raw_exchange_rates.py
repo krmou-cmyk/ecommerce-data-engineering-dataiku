2b9c7cb29c37000c5f450144ccce8b38424062de
krm.oussaidi@gmail.com <krm.oussaidi@gmail.com>
2026-09-08 13:40:20 +0000

    Saved recipe: ECOMMERCE_DATA_ENGINEERING.compute_raw_exchange_rates

--- recipes/compute_raw_exchange_rates.py
+++ recipes/compute_raw_exchange_rates.py
@@ -1,4 +1,6 @@
 import requests
+import pandas as pd
+import dataiku
 
 url = "https://api.frankfurter.dev/v1/latest?base=BRL&symbols=EUR"
 
@@ -7,4 +9,12 @@ response.raise_for_status()
 
 data = response.json()
 
-print(data)
+df = pd.DataFrame([{
+    "date": data["date"],
+    "base_currency": data["base"],
+    "target_currency": "EUR",
+    "exchange_rate": data["rates"]["EUR"]
+}])
+
+output_dataset = dataiku.Dataset("raw_exchange_rates")
+output_dataset.write_with_schema(df)
